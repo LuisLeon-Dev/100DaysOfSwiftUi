3d782df057389b472d3090f383f874eb6563ff1b
LuisLeon-Dev <lleon5073@gmail.com>
2022-08-08 05:40:13 -0600

    Converter °C to °F

---
+++ Checkpoint1.playground/Contents.swift
@@ -0,0 +1,7 @@
+import Cocoa
+
+//Temperture Convert - Celsius to Fahrenheit
+
+let celsiusTemperture = 32.0
+let fahrenheitTemperture = (celsiusTemperture * 9/5) + 32
+print("\(celsiusTemperture)°C is equal to \(fahrenheitTemperture)°F")
